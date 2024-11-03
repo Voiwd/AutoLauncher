0d94cc4ab5779bc7d61271087697cb7d5e8adf73
Voiwd <brunohenriquemoreiraa@gmail.com>
2024-11-03 03:15:22 -0300

    Basic Structure added

--- Code.py
+++ Code.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 # Color palette
 light1 = "#60C7FB"
@@ -46,6 +47,11 @@ def go_to_sandbox():
     Sandbox_Screen.place(relheight=1, relwidth=1)
 
 
+def go_to_home():
+    Sandbox_Screen.place_forget()
+    Home_Screen.place(relheight=1, relwidth=1)
+
+
 more_button = Button(window, text="+", font=("Arial", 12,
                      "bold"), width=1, height=1, command=go_to_sandbox)
 edit_button = Button(window, text="Edit", font=(
@@ -75,7 +81,8 @@ tab_New.pack(side=LEFT, padx=10, pady=5)
 tab_Preferences.pack(side=LEFT, padx=10, pady=5)
 
 # Back Button
-backButton = Button(tab_row, text="Back to Home", bg=dark1, fg=light2)
+backButton = Button(tab_row, text="Back to Home", bg=dark1,
+                    fg=light2, command=go_to_home)
 backButton.pack(side=RIGHT, padx=10, pady=5)
 
 # Workspace
@@ -84,14 +91,14 @@ properties_container = Frame(
 properties_container.grid(row=0, column=0, sticky="ns")
 
 workspace_center = Frame(workspace, bg=dark2,
-                       highlightbackground=dark1, highlightthickness=1)
+                         highlightbackground=dark1, highlightthickness=1)
 workspace_center.grid(row=0, column=1, sticky="nsew")
 
-explorer_container = Canvas(
-    workspace, width=150, bg=dark2, highlightbackground=dark1, highlightthickness=1)
+explorer_container = Frame(
+    workspace, width=200, bg=dark2, highlightbackground=dark1, highlightthickness=1)
 explorer_container.grid(row=0, column=2, sticky="ns")
 
-# Properties
+# *Properties*
 
 # Name
 name_entry = Entry(properties_container, width=20, bg=dark1, fg=light2)
@@ -109,10 +116,38 @@ prptsFrame.configure(yscrollcommand=Scrollbar.set)
 scrollbar.pack(side='left', fill=Y, padx=(5, 10), pady=5)
 
 
-#App Space
+# *App Space*
 
-app_space_topbar = Frame(workspace_center, bg=dark2, highlightbackground=dark1, highlightthickness=1, height=50, width=300)
-app_space_topbar.pack(side=LEFT, padx=10, expand=True)
+# Topbar
+launch_methods = ["Simple", "Timed"]
+top_bar = Frame(workspace_center, bg=dark1)
+top_bar.pack(side=TOP, padx=10, pady=10, fill=BOTH)
 
-# Execute a aplicação
+# Type
+type = ttk.Combobox(top_bar, values=launch_methods)
+type.set(launch_methods[0])
+type.pack(side='left', padx=5, pady=5)
+
+# Desk
+desk = Frame(workspace_center, bg=dark2,
+             highlightbackground=dark1, highlightthickness=1)
+desk.pack(side=TOP, padx=10, pady=(10, 20), fill=BOTH, expand=True)
+
+# *Explorer*
+
+# Search and Files Order
+search_bar = Entry(explorer_container, bg=dark1, fg=light2)
+search_bar.place(anchor='center', relwidth=0.6, y=20, x=80)
+
+add_path = Button(explorer_container, bg=dark1, text="+", width=1, height=1)
+add_path.place(anchor=CENTER, x=160, y=20)
+
+order_button = Button(explorer_container, bg=dark1,
+                      text="Tt", width=1, height=1)
+order_button.place(anchor=CENTER, x=180, y=20)
+
+# Files
+files = Canvas(explorer_container, bg=dark2)
+files.place(width=180, height=365, y=60, x=10)
+
 window.mainloop()

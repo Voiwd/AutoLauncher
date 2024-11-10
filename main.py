9e82e80aca368696ab38d7f3cc2d403b83ce7ca6
Voiwd <brunohenriquemoreiraa@gmail.com>
2024-11-09 21:12:43 -0300

    Desenvolvimento do Explorer
    Explorer development

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ light2 = "#FFFFFF"
 dark1 = "#2E2E2E"
 dark2 = "#1C1C1C"
 
+def remove_focus(event):
+    window.focus_set()
+
 # Window
 window = Tk()
 window.geometry("1000x600")
@@ -60,12 +63,15 @@ more_btn = ttk.Button(window, text="+", width=20, command=go_to_sandbox, image=p
 
 edit_btn = ttk.Button(window, width=20, image=edit_tk, compound="center").place(
     in_=task_container, relx=.46, y=-50, anchor="ne")
+
 # **Sandbox Screen**
 
 # Tabs
 notebook = ttk.Notebook(Sandbox_Screen)
 tab1 = ttk.Frame(notebook)
 tab2 = ttk.Frame(notebook)
+
+notebook.bind("<<NotebookTabChanged>>", remove_focus)
 
 notebook.add(tab1, text="New Task")
 notebook.add(tab2, text="Preferences")
@@ -75,16 +81,20 @@ notebook.place(relwidth=1, relheight=1)
 screen = ttk.Frame(tab1)
 screen.pack(fill=BOTH, expand=True)
 
+screen.bind("<ButtonRelease-1>", remove_focus)
+
 # properties
-name_entry = ttk.Entry(tab1, width=15).place(x=35, y=20)
+name_entry = ttk.Entry(tab1, width=15)
+name_entry.place(x=35, y=20)
 
-prop_frame = ttk.Labelframe(
-    tab1, width=175, height=470, text="Properties").place(x=20, y=60)
-
+prop_frame = ttk.Labelframe(tab1, width=175, height=470, text="Properties")
+prop_frame.place(x=20, y=60)
+prop_frame.bind("<ButtonRelease-1>", remove_focus)
 
 # workspace
 workspace = ttk.Labelframe(tab1, text="Workspace")
 workspace.place(x=210, y=60, height=470, width=600)
+workspace.bind("<ButtonRelease-1>", remove_focus)
 
 toolsbar = ttk.Frame(tab1).place(
     width=630, height=50, x=210)
@@ -95,27 +105,54 @@ typeCBox = ttk.Combobox(tab1, values=modes, width=16)
 typeCBox.set(modes[0])
 typeCBox.place(x=220, y=15)
 
-delete_tool = ttk.Button(tab1, width=4, style="Accent.TButton",
-                         image=minus_tk, compound="center").place(x=750, y=15)
-save_tool = ttk.Button(tab1, text="Save").place(x=690, y=15, )
-clear_tool = ttk.Button(tab1, text="Clear All").place(x=610, y=15, )
-import_tool = ttk.Button(
-    tab1, text="Import Task").place(x=509, y=15, )
+delete_tool = ttk.Button(tab1, width=4, style="Accent.TButton",image=minus_tk, compound="center")
+delete_tool.place(x=750, y=15)
+
+save_tool = ttk.Button(tab1, text="Save")
+save_tool.place(x=690, y=15, )
+clear_tool = ttk.Button(tab1, text="Clear All")
+clear_tool.place(x=610, y=15, )
+import_tool = ttk.Button(tab1, text="Import Task")
+import_tool.place(x=509, y=15, )
+
+delete_tool.bind("<ButtonRelease-1>", remove_focus)
+save_tool.bind("<ButtonRelease-1>", remove_focus)
+clear_tool.bind("<ButtonRelease-1>", remove_focus)
+import_tool.bind("<ButtonRelease-1>", remove_focus)
 
 # explorer
 
-search_bar = ttk.Entry(tab1, width=16).place(x=825, y=20)
-sort_btn = ttk.Button(tab1, text="Sortering",
-                      image=sort_tk).place(x=825, y=55)
+search_bar = ttk.Entry(tab1, width=16)
+search_bar.place(x=825, y=20)
+sort_btn = ttk.Button(tab1, text="Sortering",image=sort_tk)
+sort_btn.place(x=825, y=55)
 
-import_btn = ttk.Button(tab1, text="Sortering",
-                        image=folder_tk).place(x=870, y=55)
+import_btn = ttk.Button(tab1, text="Sortering",image=folder_tk)
+import_btn.place(x=870, y=55)
 
-add_btn = ttk.Button(tab1,
-                     image=plus_tk, width=4, compound="center", style="Accent.TButton").place(x=920, y=55)
+add_btn = ttk.Button(tab1,image=plus_tk, width=4, compound="center", style="Accent.TButton")
+add_btn.place(x=920, y=55)
 
-explorer = ttk.Labelframe(tab1, text="Explorer").place(
-    width=150, height=440, x=825, y=90)
+sort_btn.bind("<ButtonRelease-1>", remove_focus)
+import_btn.bind("<ButtonRelease-1>", remove_focus)
+add_btn.bind("<ButtonRelease-1>", remove_focus)
+
+explorer = ttk.Labelframe(tab1, text="Explorer")
+explorer.place(width=150, height=440, x=825, y=90)
+
+items = []
+filesLB = Listbox(explorer, selectmode="extended")
+
+
+def refresh():
+    filesLB.delete(0, END)
+    for i in items:
+        filesLB.insert(END, i)
+
+
+refresh()
+
+filesLB.place(y=1, width=150, height=425)
 
 # Exe
 sv_ttk.use_dark_theme()

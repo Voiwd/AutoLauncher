bec26db4ce58b770259c8fddc2554b3e7c4275d9
Voiwd <brunohenriquemoreiraa@gmail.com>
2024-11-23 03:32:46 -0300

    Implementar Deleção de Arquivos do explorer e correção de bugs relacionados à Workspace
    Implemented Explorer File Deletion feature and fixed bugs related to Workspace

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ from lib.logic import get_icon, get_file_dox, verify_existed_file
 from PIL import Image, ImageTk
 import sv_ttk
 import json
-
 with open("lib/log.json", "r") as f:
     data = json.load(f)
 
 # Constants
 applist = []
 
-MAX_COLS = 5
+MAX_COLS = 4
 current_row = 0
 current_col = 0
 
-
 def remove_focus(event):
     window.focus_set()
-
 
 # Window
 window = Tk()
@@ -31,13 +28,13 @@ window.iconphoto(False, icon)
 # Styles
 edit_img = Image.open("lib/source/edit.png").resize((15, 15))
 plus_img = Image.open("lib/source/plus.png").resize((10, 10))
-sort_img = Image.open("lib/source/sort-az.png").resize((15, 15))
+trash_img = Image.open("lib/source/trash-can.png").resize((15, 15))
 folder_img = Image.open("lib/source/folder.png").resize((15, 15))
 minus_img = Image.open("lib/source/minus.png").resize((10, 10))
 
 edit_tk = ImageTk.PhotoImage(edit_img)
 plus_tk = ImageTk.PhotoImage(plus_img)
-sort_tk = ImageTk.PhotoImage(sort_img)
+trash_tk = ImageTk.PhotoImage(trash_img)
 folder_tk = ImageTk.PhotoImage(folder_img)
 minus_tk = ImageTk.PhotoImage(minus_img)
 
@@ -57,16 +54,13 @@ notice.place(relx=0.5, rely=0.5, anchor="center")
 
 # Buttons
 
-
 def go_to_sandbox():
     Home_Screen.place_forget()
     Sandbox_Screen.place(relheight=1, relwidth=1)
 
-
 def go_to_home():
     Sandbox_Screen.place_forget()
     Home_Screen.place(relheight=1, relwidth=1)
-
 
 more_btn = ttk.Button(window, text="+", width=20,
                       command=go_to_sandbox, image=plus_tk, compound="center")
@@ -97,21 +91,38 @@ screen.bind("<ButtonRelease-1>", remove_focus)
 # *Explorer
 
 # Methods
-
-
 def refresh_tv(treeview, dic):
     for item in treeview.get_children():
         treeview.delete(item)
 
     for k in dic:
         shortname = k
-        # Verifica se o shortname excede 16 caracteres
         if len(shortname) > 16:
             shortname = shortname[:16] + "..."
 
         treeview.insert("", "end", values=(
             shortname, dic[k]["Name"], dic[k]["Extension"], dic[k]["Adress"]))
 
+def delete_file():
+    itens_selecionados = tv.selection()  
+
+    if not itens_selecionados:
+        print("Nenhum item selecionado.")
+        return
+
+    for item_id in itens_selecionados:
+        values = tv.item(item_id, "values")
+        shortname = values[1] if values else None
+
+        if shortname and shortname in data:
+            del data[shortname]
+
+            tv.delete(item_id)
+
+    with open("lib/log.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("Itens deletados com sucesso.")
 
 def import_file():
     filelist = get_file_dox()
@@ -130,9 +141,6 @@ def import_file():
 
     refresh_tv(tv, data)
 
-# Widgets
-
-
 def pull_to_workspace(tv: ttk.Treeview):
     focused_item = tv.focus()
     if focused_item:
@@ -140,16 +148,18 @@ def pull_to_workspace(tv: ttk.Treeview):
 
         name = itemlog[1]
         ext = itemlog[2]
-        path = itemlog[3]  
-        
+        path = itemlog[3]
+
         add_element(name, ext, path)
     else:
         print("Nenhum item está ativo.")
 
+# Widgets
 search_bar = ttk.Entry(tab1, width=19)
 search_bar.place(x=805, y=20)
-sort_btn = ttk.Button(tab1, text="Sortering", image=sort_tk)
-sort_btn.place(x=805, y=55)
+trash_btn = ttk.Button(tab1, text="Sortering",
+                       image=trash_tk, command=delete_file)
+trash_btn.place(x=805, y=55)
 
 file_btn = ttk.Button(tab1, text="Add File", image=folder_tk,
                       command=import_file)
@@ -159,7 +169,7 @@ add_btn = ttk.Button(tab1, image=plus_tk, width=4,
                      compound="center", style="Accent.TButton", command=lambda: pull_to_workspace(tv))
 add_btn.place(x=920, y=55)
 
-sort_btn.bind("<ButtonRelease-1>", remove_focus)
+trash_btn.bind("<ButtonRelease-1>", remove_focus)
 file_btn.bind("<ButtonRelease-1>", remove_focus)
 add_btn.bind("<ButtonRelease-1>", remove_focus)
 
@@ -172,14 +182,9 @@ tv.place(width=170, height=440, x=805, y=90)
 refresh_tv(tv, data)
 
 # *Workspace
-cont = ttk.Labelframe(tab1, text="Workspace")
-cont.place(x=210, y=60, height=470, width=580)
 
-canvas = Canvas(cont)
-canvas.pack(fill=BOTH, expand=True, padx=1, pady=(0, 3))
+# Methods
 
-canvas_frame = Frame(canvas)
-canvas.create_window((0, 0), window=canvas_frame, anchor="nw")
 
 def add_element(name, ext, path):
     global current_row, current_col
@@ -188,51 +193,59 @@ def add_element(name, ext, path):
         current_col = 0
         current_row += 1
 
-    # Criação do item que será adicionado à app_list
     item = {
-        "id": len(applist),  
+        "id": len(applist),
         "name": name,
         "ext": ext,
         "path": path
     }
 
-    # Adicionando o item na lista
     applist.append(item)
 
     element = ttk.Frame(canvas_frame)
-    element.grid(row=current_row, column=current_col, padx=8, pady=10)
+    element.grid(row=current_row, column=current_col, padx=20, pady=15)
 
     url = get_icon(ext)
-    image = ImageTk.PhotoImage(Image.open(url).resize((90, 90)))
+    image = ImageTk.PhotoImage(Image.open(url).resize((100, 100)))
 
     lbl = Label(element, image=image)
     lbl.image = image
-    lbl.pack(padx=2, pady=2)
+    lbl.pack(pady=2, side=TOP)
 
-    text = ttk.Label(element, text=name, font=("Arial", 13), background="gray", padding=(5, 0))
-    text.pack(padx=2, pady=10)
+    if len(name) > 10:
+        name = name[:9] + "..."
+
+    text = Label(element, text=name, background="gray")
+    text.pack(pady=10, side=BOTTOM)
 
     text.bind("<MouseWheel>", on_mouse_wheel)
     element.bind("<MouseWheel>", on_mouse_wheel)
     lbl.bind("<MouseWheel>", on_mouse_wheel)
 
-    # Atualiza o canvas
     current_col += 1
     canvas_frame.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
 
+
 def on_mouse_wheel(event):
-    if current_row <= 2:
+    if current_row <= 1:
         return
     delta = -1 * (event.delta // 120)
 
     canvas.yview_scroll(delta, "units")
 
 
-canvas_frame.bind("<MouseWheel>", on_mouse_wheel)
+# Widgets
+cont = ttk.Labelframe(tab1, text="Workspace")
+cont.place(x=210, y=60, height=470, width=580)
 
-add_btn = ttk.Button(tab1, text="Adicionar Elemento",)
-add_btn.place(x=250, y=450)
+canvas = Canvas(cont)
+canvas.pack(fill=BOTH, expand=True, padx=1, pady=(0, 3))
+
+# Config
+canvas_frame = Frame(canvas)
+canvas.create_window((0, 0), window=canvas_frame, anchor="nw")
+canvas_frame.bind("<MouseWheel>", on_mouse_wheel)
 
 # Operations
 modes = ["Default (Unordered)", "Timed"]
@@ -241,7 +254,7 @@ typeCBox.set(modes[0])
 typeCBox.place(x=220, y=15)
 
 delete_tool = ttk.Button(
-    tab1, width=4, style="Accent.TButton", image=minus_tk, compound="center")
+    tab1, width=4, style="Accent.TButton", image=trash_tk, compound="center")
 delete_tool.place(x=730, y=15)
 
 save_tool = ttk.Button(tab1, text="Save")
